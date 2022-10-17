1ff4c76688d73b47225a8389325ab27da40c903b
BENDER <88bender88@gmail.com>
2022-10-18 01:31:44 +0300

    4.1. Разбор первой программы. [#159033 #99687]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static  void main (String[] args) {
+        int x = 1;
+        for (int y = 1; y<=10; y++)
+
+            System.out.println(x + "*" + y + "=" + (x * y) + "\t");
+        }
+    }
