147d74d01fd2effc5bd88ba4e0eaa5f7825d3e30
BENDER <88bender88@gmail.com>
2022-10-17 22:52:05 +0300

    test3

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("BENDER FOREVE");
+        System.out.println("Hi BENDER");
     }
 }
