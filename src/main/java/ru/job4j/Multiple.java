e120b894057b79140ced5e885a1bc0ced4f6793f
BENDER <88bender88@gmail.com>
2022-12-09 21:02:05 +0300

    4.1. Разбор первой программы. [#159033 #99687]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,10 +1,15 @@
 package ru.job4j;
 
 public class Multiple {
-    public static  void main (String[] args) {
-        byte x = 1;
-        for (byte y = 1; y<=10; y++)
-
-            System.out.println(x + " * " + y + " = " + (x * y) + "\t");
-        }
+    public static void main(String[] args) {
+        System.out.println("1 * 9 = 9");
+        System.out.println("2 * 9 = 18");
+        System.out.println("3 * 9 = 27");
+        System.out.println("4 * 9 = 36");
+        System.out.println("5 * 9 = 45");
+        System.out.println("6 * 9 = 54");
+        System.out.println("7 * 9 = 63");
+        System.out.println("8 * 9 = 72");
+        System.out.println("9 * 9 = 81");
     }
+}
