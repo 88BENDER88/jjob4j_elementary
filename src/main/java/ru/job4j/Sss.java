9af3bae62ed4a5a8a9726e2a11b5fd4c145d910f
BENDER <88bender88@gmail.com>
2023-01-05 20:35:23 +0300

    4.1. Разбор первой программы. [#159033 #99687]

--- src/main/java/ru/job4j/Sss.java
+++ src/main/java/ru/job4j/Sss.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import ru.job4j.calculator.Calculator;
+
 public class Sss {
     public static void sss(String[] args) {
             int size = 10;
@@ -7,4 +9,11 @@ public class Sss {
             size = size - 5;
             System.out.println(size);
     }
+    /*public static void plus(int first, int second, int three) {
+        int result = first + second + three;
+        System.out.println (result);
+    }
+    public static void main(String[] args) {
+        Calculator.plus(100, 500, 4);
+        Calculator.plus (2,3,5); */
 }
