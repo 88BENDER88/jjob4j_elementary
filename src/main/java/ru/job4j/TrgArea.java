5dc9275f36271bcb1c48eebc1d5537aa4f9ff9d8
BENDER <88bender88@gmail.com>
2023-03-14 01:40:13 +0300

    4.1. Разбор первой программы. [#159033 #99687]

---
+++ src/main/java/ru/job4j/TrgArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double period = (a + b + c) / 2;
+        double rsl = Math.sqrt(period * (period - a) * (period - b) * (period - c));
+        return rsl;
+    }
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 3, 4);
+        System.out.println("area (2, 3, 4) = " + rsl);
+    }
+}
