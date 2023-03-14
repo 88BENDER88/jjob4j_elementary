bdd0ac7a7e6618be16cffe47f3b1d4d724d4afd4
BENDER <88bender88@gmail.com>
2023-03-14 11:30:54 +0300

    4.1. Разбор первой программы. [#159033 #99687]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea = "I like Java!";
+    int year;
+    year = 2023;
+    idea = idea + " But I am newbie " + year;
+
+    System.out.println(idea);
+    }
+}
