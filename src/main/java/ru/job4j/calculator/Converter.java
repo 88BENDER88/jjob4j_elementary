09fec92e46828ed5fcaf70f133a9df8f7cc6858b
Admin <proxy88bender@gmail.com>
2025-06-24 10:01:04 +0300

    добавил ручной тест

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -8,15 +8,20 @@ public class Converter {
     }
 
     public static float rubletoDollar(float value) {
-        float rsl = value / 60;
+        float rsl = value / 70;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubletoEuro(150);
+        float euro = Converter.rubletoEuro(140);
         float dollar = Converter.rubletoDollar(175);
-        System.out.println("150 rubles is " + euro + " euro.");
+        System.out.println("140 rubles is " + euro + " euro.");
         System.out.println("175 rubles is " + dollar + " dollar. ");
+        float input = 140;
+        float expected =2;
+        float output = Converter.rubletoEuro(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 
 }
