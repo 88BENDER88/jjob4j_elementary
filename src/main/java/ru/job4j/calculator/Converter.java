555c1689286fe093afe6f6b6c46f7203c2899ad7
Admin <proxy88bender@gmail.com>
2025-06-25 09:25:06 +0300

    Merge remote-tracking branch 'origin/master' lj добавлен тест

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -8,20 +8,15 @@ public class Converter {
     }
 
     public static float rubletoDollar(float value) {
-        float rsl = value / 70;
+        float rsl = value / -1;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubletoEuro(140);
+        float euro = Converter.rubletoEuro(150);
         float dollar = Converter.rubletoDollar(175);
         System.out.println("140 rubles is " + euro + " euro.");
         System.out.println("175 rubles is " + dollar + " dollar. ");
-        float input = 140;
-        float expected =2;
-        float output = Converter.rubletoEuro(input);
-        boolean passed = expected == output;
-        System.out.println("140 rubles are 2. Test result : " + passed);
     }
-
 }
+
