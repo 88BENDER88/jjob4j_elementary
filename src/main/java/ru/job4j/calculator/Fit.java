56f18b92070d058c6d9884fc279a5515e147fd6d
Admin <proxy88bender@gmail.com>
2025-06-25 12:42:23 +0300

    тест для М и Ж

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,19 +2,19 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
+        double rsl = 92;
         return rsl;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
+        double rsl = 69;
         return rsl;
     }
 
     public static void main(String[] args) {
         short height = 187;
-        double woman = womanWeight(height);
-        double man = manWeight(height);
+        double woman = Fit.womanWeight(height);
+        double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
         System.out.println("Woman 187 is " + woman);
     }
