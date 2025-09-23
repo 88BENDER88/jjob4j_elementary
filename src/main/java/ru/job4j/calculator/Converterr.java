2c6b847972b6cc81f1e31c9445375940b506995c
BENDER <proxy88bender@gmail.com>
2025-09-23 15:27:19 +0300

    Добавлен тест для конвертора

---
+++ src/main/java/ru/job4j/calculator/Converterr.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculator;
+
+public class Converterr {
+
+    public static float rubletoEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubletoEuro(140);
+        System.out.println("140 rubles is " + euro + " euro.");
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubletoEuro(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+    }
+
+}
