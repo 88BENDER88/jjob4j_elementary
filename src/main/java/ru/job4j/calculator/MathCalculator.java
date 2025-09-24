33710d2cfb01a899bdeb5061ad5aa527223795fc
BENDER <proxy88bender@gmail.com>
2025-09-24 17:16:09 +0300

    изменения отступ

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,17 +3,17 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-    public static double SumAndMultiply(double first, double second) {
+    public static double sumAndMultiply(double first, double second) {
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
 
-    public static double SumMinus(double first, double second) {
+    public static double sumMinus(double first, double second) {
         return ru.job4j.math.MathFunction.minus(first, second)
                 + ru.job4j.math.MathFunction.division(first, second);
     }
 
-    public static double SuperSum(double first, double second) {
+    public static double superSum(double first, double second) {
         return ru.job4j.math.MathFunction.sum(first, second)
                 + ru.job4j.math.MathFunction.multiply(first, second)
                 + ru.job4j.math.MathFunction.minus(first, second)
@@ -21,9 +21,9 @@ public class MathCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчёта равен: " + SumAndMultiply(10, 20));
-        System.out.println("Результат расчёта равен: " + SumMinus(10, 20));
-        System.out.println("Результат расчёта равен: " + SuperSum(10, 20));
+        System.out.println("Результат расчёта равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчёта равен: " + sumMinus(10, 20));
+        System.out.println("Результат расчёта равен: " + superSum(10, 20));
     }
 
 }
