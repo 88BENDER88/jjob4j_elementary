fffff7e599935f6705cb032184e8ad6463379e13
BENDER <proxy88bender@gmail.com>
2025-09-24 10:43:16 +0300

    изменения

--- src/main/java/ru/job4j/calculator/Converterr.java
+++ src/main/java/ru/job4j/calculator/Converterr.java
@@ -3,8 +3,8 @@ package ru.job4j.calculator;
 public class Converterr {
 
     public static float rubletoEuro(float value) {
-        float result = value / 70;
-        return result;
+        float rsl = value / 70;
+        return rsl;
     }
 
     public static float rubleToDollar(float value) {
@@ -15,7 +15,7 @@ public class Converterr {
         float euro = Converter.rubletoEuro(140);
         System.out.println("140 rubles is " + euro + " euro.");
         float input = 140;
-        float expected = 2;
+        float expected =2;
         float output = Converter.rubletoEuro(input);
         boolean passed = expected == output;
         System.out.println("140 rubles are 2. Test result : " + passed);
