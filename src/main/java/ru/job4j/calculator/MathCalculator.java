c81cfcacf85b16f2649ff10d0570d6d8c1124b54
BENDER <proxy88bender@gmail.com>
2025-09-25 13:47:14 +0300

    сократил полное название пакета (импортируемого)

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,23 +1,23 @@
 package ru.job4j.calculator;
 
-import static ru.job4j.math.MathFunction.*;
+import ru.job4j.math.MathFunction;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second);
+        return MathFunction.sum(first, second)
+                + MathFunction.multiply(first, second);
     }
 
     public static double sumMinus(double first, double second) {
-        return ru.job4j.math.MathFunction.minus(first, second)
-                + ru.job4j.math.MathFunction.division(first, second);
+        return MathFunction.minus(first, second)
+                + MathFunction.division(first, second);
     }
 
     public static double superSum(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second)
-                + ru.job4j.math.MathFunction.minus(first, second)
-                + ru.job4j.math.MathFunction.division(first, second);
+                return MathFunction.sum(first, second)
+                + MathFunction.multiply(first, second)
+                + MathFunction.minus(first, second)
+                + MathFunction.division(first, second);
     }
 
     public static void main(String[] args) {
